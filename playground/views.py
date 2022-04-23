31c7b6314601b6c06727f752c6d7b73efac53fe9
Neeraj-SD <neerajsd04@gmail.com>
2022-04-23 11:10:34 +0530

    retrieving objects

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-
-    for product in query_set:
-        print(product)
+    try:
+        product = Product.objects.filter(pk=0).first()
+    except ObjectDoesNotExist:
+        pass
 
     return render(request, 'hello.html', {'name': 'Mosh'})
